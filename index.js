a9c7ecd653e99d1fcc15ce7edf76ebaf31ad2987
Elvis Kemoi <elviskemoi001@gmail.com>
2024-04-21 18:36:42 +0300

    Firs commit on file uploads

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const multer = require("multer");
+const cors = require("cors");
+
+var app = express();
+app.use(cors()); // Allows incoming requests from any IP
+
+app.use(express.static("public"));
+
+// Start by creating some disk storage options:
+const storage = multer.diskStorage({
+	destination: function (req, file, callback) {
+		callback(null, __dirname + "/uploads");
+	},
+	// Sets file(s) to be saved in uploads folder in same directory
+	filename: function (req, file, callback) {
+		callback(null, file.originalname);
+	},
+	// Sets saved filename(s) to be original filename(s)
+});
+
+app.get("/", (req, res) => {
+	res.status(200).sendFile(__dirname + "/index.html");
+});
+
+// Set saved storage options:
+const upload = multer({ storage: storage });
+
+app.post("/api", upload.array("files"), (req, res) => {
+	// Sets multer to intercept files named "files" on uploaded form data
+
+	console.log(req.body); // Logs form body values
+	console.log(req.files); // Logs any files
+	res.json({ message: "File(s) uploaded successfully" });
+});
+
+app.listen(5000, function () {
+	console.log("Server running on port 5000");
+});
